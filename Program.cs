f72efd237d833230e5c539c46451a57657eb0ded
Torsten Franz <torsten_franz@agilent.com>
2018-06-12 13:49:06 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿namespace QuotesCheck
+{
+    using System;
+
+    using Avapi;
+    using Avapi.AvapiEMA;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Updating symbol provider");
+            var symbolProvider = new SymbolProvider();
+            symbolProvider.UpdateReference();
+
+
+            return;
+            // Creating the connection object
+            IAvapiConnection connection = AvapiConnection.Instance;
+
+            // Set up the connection and pass the API_KEY provided by alphavantage.co
+            connection.Connect("XS7IY6V9YRY2SL15");
+
+            // Get the TIME_SERIES_DAILY query object
+            var time_series_daily =
+                connection.GetQueryObject_EMA();
+
+            // Perform the TIME_SERIES_DAILY request and get the result
+            var time_series_dailyResponse =
+            time_series_daily.Query(
+                 "A", Const_EMA.EMA_interval.daily, 20, Const_EMA.EMA_series_type.close);
+
+            // Printout the results
+            Console.WriteLine("******** RAW DATA TIME_SERIES_DAILY ********");
+            Console.WriteLine(time_series_dailyResponse.RawData);
+
+            
+        }
+    }
+}
---
+++ SymbolProvider.cs
@@ -0,0 +1,24 @@
+﻿namespace QuotesCheck
+{
+    using System;
+    using System.IO;
+    using System.Net;
+
+    internal class SymbolProvider
+    {
+        private const string referencePath = @"ReferenceData\BXESymbols-PROD.csv";
+
+        private const string tempReferencePath = @"ReferenceData\BXESymbols-TEMP.csv";
+
+        public void UpdateReference()
+        {
+            using (var wc = new WebClient())
+            {
+                wc.DownloadFile(new Uri("https://batstrading.co.uk/bxe/market_data/symbol_listing/csv/"), tempReferencePath);
+            }
+
+            File.Delete(referencePath);
+            File.Move(tempReferencePath, referencePath);
+        }
+    }
+}
